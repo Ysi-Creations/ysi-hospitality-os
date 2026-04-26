17c4232d252966077bb19229a13e629869517aff
Ysi-Creations <Ysi-Creations@outlook.com>
2026-04-26 20:10:06 +0100

    Fix Supabase URL format in App.jsx

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https:/ayotgjvtivoyfpdwhrud/.supabase.co",
+  "https://ayotgjvtivoyfpdwhrud.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5b3RnanZ0aXZveWZwZHdocnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNjUyNjEsImV4cCI6MjA5MjY0MTI2MX0._sbVpoN-gtxVjrCUkqC2N3S-cerzkvmLRnKY0zv9TGs"
 );
 
