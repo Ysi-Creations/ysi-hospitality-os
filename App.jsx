2510cf7ef283a9dd0ffb40194b60d59d6ec9c42d
Ysi-Creations <Ysi-Creations@outlook.com>
2026-05-01 11:06:50 +0100

    Replace routing with a test heading

--- App.jsx
+++ App.jsx
@@ -1,18 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Bar from "./Bar";
-
-// we will add these next
-const Ordering = () => <h1>Ordering screen coming</h1>;
-const Kitchen = () => <h1>Kitchen screen coming</h1>;
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Ordering />} />
-        <Route path="/kitchen" element={<Kitchen />} />
-        <Route path="/bar" element={<Bar />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>ROUTER TEST</h1>;
 }
