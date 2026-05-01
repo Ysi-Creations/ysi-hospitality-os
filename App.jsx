1e57f688b44dba698f7b7670046674754ac1ad19
Ysi-Creations <Ysi-Creations@outlook.com>
2026-05-01 10:53:12 +0100

    Add routing for Ordering and Kitchen screens

--- App.jsx
+++ App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bar from "./Bar";
 
+// we will add these next
+const Ordering = () => <h1>Ordering screen coming</h1>;
+const Kitchen = () => <h1>Kitchen screen coming</h1>;
+
 export default function App() {
-  return <Bar />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Ordering />} />
+        <Route path="/kitchen" element={<Kitchen />} />
+        <Route path="/bar" element={<Bar />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
