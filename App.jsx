3f412cfbbf391294e2b09bca2dccbb47903a2ea4
Ysi-Creations <Ysi-Creations@outlook.com>
2026-05-01 15:44:32 +0300

    Replace routing logic with a test message

--- App.jsx
+++ App.jsx
@@ -1,23 +1,3 @@
-import React from "react";
-import Ordering from "./Ordering";
-import Kitchen from "./Kitchen";
-import Bar from "./Bar";
-import Admin from "./Admin";
-
 export default function App() {
-  const path = typeof window !== "undefined" ? window.location.pathname : "/";
-
-  switch (path) {
-    case "/kitchen":
-      return <Kitchen />;
-
-    case "/bar":
-      return <Bar />;
-
-    case "/admin":
-      return <Admin />;
-
-    default:
-      return <Ordering />;
-  }
+  return <h1>BUILD TEST - OK</h1>;
 }
