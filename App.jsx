bfbd61d11ddcb0b1fc4fda363dc633058d042eee
Ysi-Creations <Ysi-Creations@outlook.com>
2026-05-01 11:09:54 +0100

    Implement routing with BrowserRouter and Routes

--- App.jsx
+++ App.jsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Bar from "./Bar";
+
+const Ordering = () => <h1>Ordering screen coming</h1>;
+const Kitchen = () => <h1>Kitchen screen coming</h1>;
+
 export default function App() {
-  return <h1>ROUTER TEST</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Ordering />} />
+        <Route path="/kitchen" element={<Kitchen />} />
+        <Route path="/bar" element={<Bar />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
